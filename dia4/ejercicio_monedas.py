53800fccfd6aed608c423b73c6d8c6414541f42d
cesar mayta <cesardatag5@gmail.com>
2025-10-10 20:38:05 -0500

    ejecicio terminado

--- dia4/ejercicio_monedas.py
+++ dia4/ejercicio_monedas.py
@@ -5,12 +5,15 @@ POR EJEMPLO SI INGRESO CONVERTIR SOLES A DOLARES E INGRESO 3000 SOLES
 DEBERIA MOSTRARME SU VALOR EN DOLARES QUE SERIA 1000 DOLARES CONSIDERANDO 
 QUE EL TIPO DE CAMBIO ES 3
 """
+import os
+from time import sleep
 # DATOS DE ENTRADA(INPUT)
 tipo_cambio = 3
 moneda_origen = "SOLES"
 moneda_destino = "DOLARES"
 
 while(True):
+    os.system("clear")
     print("""
             =============================================
                         CONVERTIDOR DE MONEDAS
@@ -24,15 +27,15 @@ while(True):
     if(opcion == 1):
         moneda_origen = "SOLES"
         moneda_destino = "DOLARES"
-        monto_origen = float(input("Ingrese el monto a convertir: "))
+        monto_origen = float(input("Ingrese el monto en SOLES : "))
         # PROCESO
-        monto_destino = monto_origen / tipo_cambio
+        monto_destino = round(monto_origen / tipo_cambio,2)
     elif(opcion == 2):
         moneda_origen = "DOLARES"
         moneda_destino = "SOLES"
-        monto_origen = float(input("Ingrese el monto a convertir: "))
+        monto_origen = float(input("Ingrese el monto en DOLARES : "))
         # PROCESO
-        monto_destino = monto_origen * tipo_cambio
+        monto_destino = round(monto_origen * tipo_cambio,2)
     elif(opcion == 3):
         print("Gracias por usar el convertidor de monedas")
         exit()
@@ -41,3 +44,4 @@ while(True):
         continue
     # DATOS DE SALIDA(OUTPUT)
     print(f"RESULTADO : {monto_origen} {moneda_origen} son {monto_destino} {moneda_destino}")
+    sleep(2)
