1b3207d9e39b6087ad951975149f880b434b9def
cesar mayta <cesarmayta@MacBook-Air-de-cesar.local>
2025-10-20 22:44:44 -0500

    importar a base de datos

--- dia3/consumo-apis/01-requests.py
+++ dia3/consumo-apis/01-requests.py
@@ -1,7 +1,9 @@
 # pip install requests
 # pip install tabulate
+# pip install mysql-connector-python
 import requests
 from tabulate import tabulate
+import mysql.connector
 
 URL = 'https://randomuser.me/api/?results=10'
 
@@ -22,6 +24,43 @@ if response.status_code == 200:
 
     headers = ['Nombre', 'País', 'Correo', 'Teléfono', 'Foto']
     print(tabulate(rows, headers=headers, tablefmt='grid'))
+
+    # Conexión a la base de datos MySQL
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root2025',
+        database='db_g5'
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS usuario(
+            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(255) NOT NULL,
+            pais VARCHAR(255) NOT NULL,
+            correo VARCHAR(255),
+            telefono VARCHAR(100),
+            foto TEXT
+            );
+            """
+        )
+        # registramos usuarios en la bd
+        for usuario in rows:
+            cursor.execute(
+                """
+                    insert into usuario(nombre,pais,correo,telefono,foto)
+                    values(%s,%s,%s,%s,%s)
+                """,
+                usuario
+            )
+
+        connection.commit()
+        print(f' registros importados a la base de datos!!!')
+    else:
+        print(f'no esta conectado a la BD')
 
 else:
     print(f'algo salio mal : {response.status_code}')
