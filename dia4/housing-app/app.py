725203ae5d8444ed0c4a8d2cef1fed9bf3ac7081
cmayta <cesarmayta@gmail.com>
2026-01-26 20:46:17 -0500

    housing app listo

--- dia4/housing-app/app.py
+++ dia4/housing-app/app.py
@@ -1,11 +1,25 @@
 from flask import Flask,request,render_template
+import joblib
+import numpy as np
+import sklearn
 
+model = joblib.load('./model/housing-ml.pkl')
+sc_x = joblib.load('./model/scaler_x.pkl')
+sc_y = joblib.load('./model/scaler_y.pkl')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    result = 0
+    if request.method == 'POST':
+        rooms = int(request.form['rooms'])
+        rooms_sc = sc_x.transform(np.array([[rooms]]))
+        prediction = model.predict(rooms_sc)
+        prediction_sc = sc_y.inverse_transform(prediction) * 1000
+        result = round(prediction_sc[0][0],2)
+        
+    return render_template('index.html',result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
