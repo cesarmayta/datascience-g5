189a2f910dd5adc30e0297bfa20e79dd22b2f30a
cmayta <cesarmayta@gmail.com>
2026-02-06 22:36:25 -0500

    housing api listo

--- dia3/housing-api/app.py
+++ dia3/housing-api/app.py
@@ -65,5 +65,62 @@ def set_data():
     
     return jsonify(context)
 
+@app.route('/housing',methods=['GET'])
+def get_data():
+    data = Housing.query.all() # select * from housing
+    data_schema = HousingSchema(many=True)
+    return jsonify(data_schema.dump(data))
+
+@app.route('/housing/<int:id>',methods=['GET'])
+def get_data_by_id(id):
+    data = Housing.query.get(id) # select * from housing where id = id
+    data_schema = HousingSchema()
+    
+    return jsonify(data_schema.dump(data)),200 if data else 404
+
+@app.route('/housing/<int:id>',methods=['PUT'])
+def update_data(id):
+    data = Housing.query.get(id)
+    if not data:
+        context = {
+            'message':'Registro no encontrado'
+        }
+        return jsonify(context),404
+    
+    region = request.json['region']
+    rooms = request.json['rooms']
+    distance = request.json['distance']
+    new_housing_model = HousingModel()
+    price = new_housing_model.predict_price(region,rooms,distance)
+    
+    data.rooms = rooms
+    data.region = region
+    data.distance = distance
+    data.price = price
+    db.session.commit()
+    
+    data_schema = HousingSchema()
+    
+    return jsonify(data_schema.dump(data)),200
+
+@app.route('/housing/<int:id>',methods=['DELETE'])
+def delete_data(id):
+    data = Housing.query.get(id)
+    
+    if not data:
+        context = {
+            'message':'Registro no encontrado'
+        }
+        return jsonify(context),404
+    
+    db.session.delete(data) #delete from housing
+    db.session.commit()
+    
+    context = {
+        'message':'Registro eliminado correctamente'
+    }
+    
+    return jsonify(context),200
+
 if __name__ == '__main__':
     app.run(debug=True)
